3c3c786bf337a1920fe1a65259bb2b1b1e65fd6e
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2015-10-09 19:31:59 -0500

    NXEngine v1.0.0.5

--- map.cpp
+++ map.cpp
@@ -413,7 +413,7 @@ static void DrawFastLeftLayered(void)
 {
 static const int layer_ys[] = { 80, 122, 145, 176, 240 };
 static const int move_spd[] = { 0,    1,   2,   4,   8 };
-int nlayers = 6;
+const int nlayers = 5;
 int y1, y2;
 int i, x;
 
